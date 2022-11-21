23a628098bf211fa5b2f3ceabc9fc52832518688
okni-c <oknistay@gmail.com>
2022-11-21 13:06:18 -0600

    Type lint error

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,8 +4,8 @@ export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   providers: [
     DiscordProvider({
-      clientId: process.env.DISCORD_CLIENT_ID,
-      clientSecret: process.env.DISCORD_CLIENT_SECRET
+      clientId: process.env.DISCORD_CLIENT_ID || 'hello',
+      clientSecret: process.env.DISCORD_CLIENT_SECRET || 'hello'
     })
   ],
 }
