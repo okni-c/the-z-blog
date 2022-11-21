5aa7343b79072e700ab22f232e42cdf779d14210
okni-c <oknistay@gmail.com>
2022-11-21 15:41:18 -0600

    Sign in auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,6 +2,15 @@ import NextAuth, { type NextAuthOptions } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord"
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
+  callbacks: {
+    async signIn({user}) {
+      if (user.email === process.env.EMAIL_AUTH) {
+        return true
+      } else {
+        return '/unauthorized'
+      }
+    }
+  },
   providers: [
     DiscordProvider({
       clientId: process.env.DISCORD_CLIENT_ID || 'hello',
