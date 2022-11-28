d86a39cdc5a857f4f96d7ef8a8056a6e188479a6
okni-c <oknistay@gmail.com>
2022-11-28 00:02:05 -0600

    Hot fix for dark mode, small image changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
         <footer className="bg-yellow-400 dark:bg-cyan-800 rounded-xl m-2 drop-shadow-xl">
             <nav className="flex mx-5 py-4 items-center">
                 <p className="text-xl font-black my-0 md:text-2xl tracking-tight"></p>
-                <Image src="/logo.jpg" width={100} height={10} className="w-12 drop-shadow-md md:w-24" alt="icon of a nissan 300zx from 1986." priority />
+                <Image src="/logo.jpg" width={100} height={100} className="w-12 drop-shadow-md md:w-24 h-auto" alt="icon of a nissan 300zx from 1986."  />
                 <ul className="flex gap-5 ml-auto">
                     <li className="font-medium flex items-center"><Link href={"https://github.com/okni-c"} legacyBehavior><a>GitHub</a></Link></li>
                 </ul>
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,8 +5,8 @@ import { FaRegSun, FaRegMoon } from 'react-icons/fa'
 import { useRouter } from "next/router";
 
 export default function Nav() {
-    const [, setDarkToggle] = useState(true);
-    const [isChecked, setIsChecked] = useState(false);
+    const [, setDarkToggle] = useState<boolean | undefined>();
+    const [isChecked, setIsChecked] = useState<boolean | undefined>();
     const [isHome, setIsHome] = useState(false)
     const router = useRouter()
 
@@ -15,6 +15,11 @@ export default function Nav() {
             setIsChecked(e.matches ? true : false)
         });
         setIsChecked(window.matchMedia('(prefers-color-scheme: dark)').matches ? true : false)
+        if (localStorage.getItem('z-blog-theme') === 'true') {
+            setIsChecked(true)
+        } else {
+            setIsChecked(false)
+        }
         if (router.pathname === '/') {
             setIsHome(true)
             return
@@ -30,12 +35,14 @@ export default function Nav() {
         if (isChecked === true) {
             root?.setAttribute("class", "dark");
             setDarkToggle(true)
+            localStorage.setItem('z-blog-theme', 'true');
             return
         }
 
         if (isChecked === false) {
             root?.setAttribute("class", "");
             setDarkToggle(false)
+            localStorage.setItem('z-blog-theme', 'false');
             return
         }
 
@@ -44,7 +51,7 @@ export default function Nav() {
     return (
         <div className="bg-yellow-400 dark:bg-cyan-800 rounded-xl m-2 drop-shadow-xl">
             <nav className="flex justify-between mx-5 py-4 items-center">
-                <p className="text-2xl font-black my-0 md:text-2xl"><Link href={"/"} legacyBehavior><a className="flex flex-row items-center"><Image src="/favicon.jpg" width={100} height={10} className="w-7 md:w-8 drop-shadow-md mr-1" alt="icon of a nissan 300zx from 1986." priority />Z-Chronicles</a></Link></p>
+                <p className="text-2xl font-black my-0 md:text-2xl"><Link href={"/"} legacyBehavior><a className="flex flex-row items-center"><Image src="/favicon.jpg" width={100} height={10} className="w-7 md:w-8 drop-shadow-md mr-1" alt="icon of a nissan 300zx from 1986."  />Z-Chronicles</a></Link></p>
                 <ul className="flex gap-5">
                     {isHome ?
                         <></>
@@ -56,7 +63,7 @@ export default function Nav() {
                             <label htmlFor="toggleB" className="flex items-center cursor-pointer">
                                 <div className="relative">
                                     <input type="checkbox" id="toggleB" className="sr-only" onChange={(event) => setIsChecked(event.currentTarget.checked)}
-                                        checked={isChecked} role='' />
+                                        checked={isChecked || false} />
                                     <div className="block dark:bg-neutral-800 bg-neutral-100 dark:bg-opacity-80 bg-opacity-80 drop-shadow-xl w-14 h-8 rounded-full"></div>
                                     <div className="dot absolute left-1 top-1 w-6 h-6 rounded-full transition flex items-center justify-center bg-yellow-400">{isChecked ? <FaRegMoon /> : <FaRegSun />}</div>
                                 </div>
